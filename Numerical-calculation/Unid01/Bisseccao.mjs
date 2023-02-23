84d5e0470bd3426cba820d2d9bd6b20f1b70c305
Lucas Vinicius Fernandes da Silva <lucassilved@gmail.com>
2023-02-23 09:06:30 -0300

    feat: implements method of bisseccao

--- Numerical-calculation/Unid01/Bisseccao.mjs
+++ Numerical-calculation/Unid01/Bisseccao.mjs
@@ -1,22 +1,33 @@
 class Bisseccao {
-  #f;
-  #equation;
-  #erro;
-
-  constructor(f, equation, erro) {
-    this.#f = f;
-    this.#equation = equation;
-    this.#erro = erro;
+  #func(x) {
+    return x * x * x - x * x + 2;
   }
 
-  f() {
-    return Math.pow(x, 2) || this.#f;
+  #bisseccao(a, b, erro, iterator) {
+    let x = 0;
+
+    for (let i = 0; i < iterator; i++) {
+      x = (a + b) / 2;
+
+      if (Math.abs(this.#func(x)) < erro) {
+        return x;
+      }
+
+      if (this.#func(a) * this.#func(x) < 0) {
+        b = x;
+      } else {
+        a = x;
+      }
+    }
+
+    return x;
   }
 
-  bisseccao() {}
-
-  calculate() {}
+  calculate(a, b, erro, iterator) {
+    const outPut = this.#bisseccao(a, b, erro, iterator);
+    console.log(`Zero da função: ${outPut}`);
+  }
 }
 
-const metodoBisseccao = new Bisseccao();
-metodoBisseccao.calculate();
+const methodBisseccao = new Bisseccao();
+methodBisseccao.calculate(-2, -1, 0.0000001, 100);
