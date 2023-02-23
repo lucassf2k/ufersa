c65bc271b3cce29a3d7f6dcbbcef0c4cf706ccbb
Lucas Vinicius Fernandes da Silva <lucassilved@gmail.com>
2023-02-23 08:30:07 -0300

    feat: add method of bisseccao

---
+++ Numerical-calculation/Unid01/Bisseccao.mjs
@@ -0,0 +1,22 @@
+class Bisseccao {
+  #f;
+  #equation;
+  #erro;
+
+  constructor(f, equation, erro) {
+    this.#f = f;
+    this.#equation = equation;
+    this.#erro = erro;
+  }
+
+  f() {
+    return Math.pow(x, 2) || this.#f;
+  }
+
+  bisseccao() {}
+
+  calculate() {}
+}
+
+const metodoBisseccao = new Bisseccao();
+metodoBisseccao.calculate();
