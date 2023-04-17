3416d5e3eefb4d53ceb476afb5d5ef3b09acde7c
lucassf2k <lucassilved@gmail.com>
2023-04-17 17:08:14 -0300

    feat: add insert sort

--- ED1/ED1.java/src/unidade03/Unid03.java
+++ ED1/ED1.java/src/unidade03/Unid03.java
@@ -17,6 +17,22 @@ public class Unid03 {
     }
   }
 
+  public static void insertSort(int[] arr) {
+    int size = arr.length;
+
+    for (int i = 1; i < size; i++) {
+      int key = arr[i];
+      int j = i - 1;
+
+      while (j >= 0 && arr[j] > key) {
+        arr[j+1] = arr[j];
+        j--;
+      }
+
+      arr[j+1] = key;
+    }
+  }
+
   public static void main(String[] args) {
     int[] vet = { 8, 5, 9, 2, 4, 6 };
 
@@ -26,7 +42,7 @@ public class Unid03 {
 
     System.out.println();
 
-    bubbleSort(vet);
+    insertSort(vet);
     for (int element : vet) {
       System.out.print(element + " ");
     }
